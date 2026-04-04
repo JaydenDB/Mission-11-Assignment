5468a08d7ca2ef12d338d2aafe76c8da7e1883e2
JaydenDB <35471045+JaydenDB@users.noreply.github.com>
2026-04-03 18:13:58 -0600

    Changes for deployment

--- BookstoreApi/Program.cs
+++ BookstoreApi/Program.cs
@@ -13,7 +13,10 @@ builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://127.0.0.1:5173")
+        policy.WithOrigins(
+                "http://localhost:5173",
+                "http://127.0.0.1:5173",
+                "https://mission-11-assignment-g8bkghhgcbamh6fg.westus3-01.azurewebsites.net")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
@@ -27,11 +30,18 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseCors();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseHttpsRedirection();
 }
+
 app.UseAuthorization();
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
